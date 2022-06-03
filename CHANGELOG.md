f07f942caeca4d21c5365dcb843006681650897d
Mats Mikkel Rummelhoff <mail@mmikkel.no>
2022-06-03 22:08:32 +0200

    Craft 4.0.4 compatability. Bump to 2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 2.0.1 - 2022-06-03
+### Fixed
+- Fixes a compatability issue introduced in Craft 4.0.4
+
 ## 2.0.0 - 2022-05-13
 ### Added
 - Craft 4.0 compatibility
--- src/FocalpointMe.php
+++ src/FocalpointMe.php
@@ -106,7 +106,7 @@ class FocalpointMe extends Plugin
             }
         );
 
-        // Prevent mousedown on asset thumbnails from opening preview modals
+        // Prevent clicks on asset thumbnails from opening preview modals
         if (Craft::$app->getRequest()->getIsCpRequest()) {
             Event::on(View::class, View::EVENT_BEFORE_RENDER_PAGE_TEMPLATE, static function (TemplateEvent $e) {
                 /** @var View $view */
@@ -117,7 +117,7 @@ class FocalpointMe extends Plugin
                             return;
                         }
                         const fn = Craft.AssetSelectInput.prototype.onAddElements;
-                        const preventPreviewModalOnMouseDown = () => $('.elementthumb.open-preview').off('mousedown touchstart');
+                        const preventPreviewModalOnMouseDown = () => $('.elementthumb.open-preview').off('click mousedown touchstart');
                         Craft.AssetSelectInput.prototype.onAddElements = function () {
                             fn.apply(this, arguments);
                             preventPreviewModalOnMouseDown();
